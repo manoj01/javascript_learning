cec7d39aacf3d6355183888db08f797921c4a248
Manoj IBM system <manojr2gmail.com>
2025-03-20 21:39:45 +0530

    type commit

---
+++ Typeconversion/ttype.js
@@ -0,0 +1,16 @@
+let x = 10;
+let y = "hellow"
+let z = true
+
+let m;
+let n 
+let o;
+
+
+
+console.log(typeof x, typeof y, typeof z)
+
+x= String(x);
+z = String(z);
+y = Number(y);
+console.log(typeof x, typeof y, typeof z, z, y)
